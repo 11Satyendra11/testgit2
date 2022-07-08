72b55eda4156cc73da55a01ef6e811bd1ce25667
satyendra <csatyendra02@gmail.com>
2022-07-08 18:56:04 +0530

    first commit

---
+++ src/main/java/com/mycompany/testgit1/Testgit2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testgit1;
+
+/**
+ *
+ * @author satyendra
+ */
+public class Testgit2 {
+    
+    Testgit2() {
+       System.out.println("TestGit2");  
+    }
+   
+}
