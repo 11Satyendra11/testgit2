43c0c26603076739ac63ac44714239276ae26da1
satyendra <csatyendra02@gmail.com>
2022-07-31 22:48:00 +0530

    newbranch3 new

--- src/main/java/com/mycompany/testgit1/Testgit3.java
+++ src/main/java/com/mycompany/testgit1/Testgit3.java
@@ -17,7 +17,7 @@ public class Testgit3 {
          System.out.println("Hello world  6");
          System.out.println("Hello world  7");
           System.out.println("Hello world  8");
-        //new branch2
+        //new branch3
           System.out.println("Hello world  16");
           System.out.println("Hello world  17");
            System.out.println("Hello world  18");
@@ -25,6 +25,16 @@ public class Testgit3 {
            System.out.println("Hello world  16");
            System.out.println("Hello world  17");
             System.out.println("Hello world  18");
+            //new branch3
+            System.out.println("Hello world  16");
+            System.out.println("Hello world  17");
+             System.out.println("Hello world  18");
+     
+             //new branch3
+             System.out.println("Hello world  16");
+             System.out.println("Hello world  17");
+              System.out.println("Hello world  18");
+      
     
     }
     Testgit3 obj1 = new Testgit3();
